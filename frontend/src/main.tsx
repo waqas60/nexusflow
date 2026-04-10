7f5daf61b2f93d5b5fb9d16d039be38d724e85a6
waqas <waqas.official2005@gmail.com>
2026-04-10 22:53:19 +0500

    fix random issues

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,9 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
-import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
-    <Analytics />
   </BrowserRouter>,
 );
