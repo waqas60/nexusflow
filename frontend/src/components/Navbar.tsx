620a09b8512099e24e3bd9ebd6ef7f8455952693
waqas <waqas.official2005@gmail.com>
2026-04-11 18:28:44 +0500

    feat: add logout btn and functionality

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,9 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import Button from "./Button";
+import { toast } from "react-toastify";
 
 export default function Navbar() {
   const navigate = useNavigate();
   const username = localStorage.getItem("username");
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("username");
+    toast.success("You've been logged out", { autoClose: 2000 });
+    setTimeout(() => navigate("/"), 2000);
+  };
 
   return (
     <nav className="sticky top-0 z-50 bg-[#f8f7f4]/90 backdrop-blur">
@@ -21,6 +29,14 @@ export default function Navbar() {
           >
             Organization
           </p>
+
+          {username && (
+            <Button
+              text="Logout"
+              className="bg-red-600 font-bold"
+              onClick={() => logout()}
+            />
+          )}
 
           {username ? (
             <div className="w-9 h-9 sm:w-10 sm:h-10 rounded-full bg-slate-900 text-white text-xs sm:text-[13px] font-bold flex items-center justify-center">
